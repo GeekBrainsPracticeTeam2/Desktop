edd08cd6c0819a3bb5651ea37f7955af1446a23d
olegilminsky <olegilminsky@yandex.ru>
2017-03-11 00:23:36 +0300

    Добавил выбор периода для вкладки "Ежедневная статистика"

--- src/DailyStats.java
+++ src/DailyStats.java
@@ -1,13 +1,18 @@
+import com.michaelbaranov.microba.calendar.DatePicker;
+
+import java.util.Date;
 import javax.swing.*;
 
-public class DailyStats extends JPanel{
+
+public class DailyStats extends JPanel {
     JLabel siteLabel = new JLabel("Сайт:");
     JLabel personLabel = new JLabel("Личность:");
     JComboBox<String> listOfSites = new JComboBox<String>();
     JComboBox<String> listOfPersons = new JComboBox<String>();
     JLabel periodLabel1 = new JLabel("Период с:");
-
+    final DatePicker startDate = new DatePicker(new Date());
     JLabel periodLabel2 = new JLabel("по:");
+    final DatePicker finishDate = new DatePicker(new Date());
     JButton getButton = new JButton("Получить");
 
     public DailyStats() {
@@ -16,7 +21,7 @@ public class DailyStats extends JPanel{
         listOfSites.setBounds(100, 10, 120, 20);
         personLabel.setBounds(20, 30, 60, 60);
         listOfPersons.setBounds(100, 50, 120, 20);
-        getButton.setBounds(260, 90, 140, 20);
+        getButton.setBounds(360, 90, 140, 20);
         listOfSites.addItem("lenta.ru");
         listOfSites.addItem("meduza.ru");
         listOfSites.addItem("ria.ru");
@@ -24,14 +29,19 @@ public class DailyStats extends JPanel{
         listOfPersons.addItem("Медведев");
         listOfPersons.addItem("Навальный");
         periodLabel1.setBounds(20, 70, 60, 60);
-        periodLabel2.setBounds(160, 70, 60, 60);
+        startDate.setBounds(100, 90, 100, 20);
+        periodLabel2.setBounds(210, 70, 60, 60);
+        finishDate.setBounds(240, 90, 100, 20);
 
         add(siteLabel);
         add(listOfSites);
         add(personLabel);
         add(listOfPersons);
         add(periodLabel1);
+        add(startDate);
         add(periodLabel2);
+        add(finishDate);
         add(getButton);
     }
+
 }
--- src/UserInterfaceWindows.java
+++ src/UserInterfaceWindows.java
@@ -4,7 +4,7 @@ public class UserInterfaceWindows extends JFrame{
     public UserInterfaceWindows() {
         super("Статистический анализ информационных сайтов");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setBounds(300, 50, 600, 600);
+        setBounds(300, 50, 700, 600);
         setResizable(false);
 
         JTabbedPane userTabPan = new JTabbedPane(2);
